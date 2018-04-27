ff2c8537a73e6b8cdcf63e15cba0c778818def77
Guilherme de Almeida Freitas <guilherme.de.almeida@ibm.com>
2018-04-27 10:17:40 -0300

    #1

---
+++ src/main/java/com/almeida/itgcosmeticos/api/ItgcosmeticosApiApplication.java
@@ -0,0 +1,12 @@
+package com.almeida.itgcosmeticos.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItgcosmeticosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItgcosmeticosApiApplication.class, args);
+	}
+}
